d5fa66e7fb91f2b9c06da7553178475db81b3f2b
Ronny <ronnybet41@gmail.com>
2022-11-09 23:43:48 +0300

    add home styling

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,22 @@
+import "./sidebar.css"
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+    <div className="sidebaritem">
+        <span className="sidebartittle">ABOUT PICKARIDE</span>
+        <img className="sidebarimg"
+            src="car5.jpeg" alt=""></img>
+        <p className="sidebarpara">Lorem ipsum dolor sit amet consectetur adipisicing elit. c
+            nulla iure! Laudantium vitae iure unde minus nihil dolore?</p>
+    </div>
+   
+    <div className="sidebaritem">
+        <span className="sidebartittle">FOLLOW US</span>
+        <i className="sidebaricon fa-brands fa-facebook"></i>
+        <i className="sidebaricon fa-brands fa-twitter"></i>
+        <i className="sidebaricon fa-brands fa-instagram"></i>
+    </div>
+</div>
+)
+}
---
+++ src/components/singlecar/Singlecar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Singlecar() {
+  return (
+    <div className="singlecar">
+    <div className="singlecarwrapper">
+        <img className="singlecarimg" src="pex2.jpg" alt="" />
+        <h1 className="singlecartittle">
+        harrier
+        </h1>
+        <div className="singlecaredit">
+            <i className="singlecaricon fa-solid fa-pen-to-square"></i>
+            <i className="singlecaricon fa-solid fa-trash"></i>
+           
+        </div>
+        <div className="singlecarinfo">
+            <span className="singlecarprice">pricer: <b>ksh. 3m</b></span>
+            <span className="singlepcarmodel">model:<b>Toyota</b></span>
+        </div>
+        <p className="singlepcardesc">Lorem ipsum, dolor sit amet consectetur adipisicing elit. In earum
+            </p>
+    </div>
+</div>
+)
+}
