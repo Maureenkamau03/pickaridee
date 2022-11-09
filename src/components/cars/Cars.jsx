3183228fcf26c2248fae2089979f3d98f842d953
Ronny <ronnybet41@gmail.com>
2022-11-09 23:42:07 +0300

    add home component

---
+++ src/components/cars/Cars.jsx
@@ -0,0 +1,26 @@
+
+import "./cars.css"
+import Car from "../car/Car"
+export default function Cars() {
+  return (
+    <div className="cars">
+      
+      <Car/>
+      <Car/>
+      <Car/>
+      <Car/>
+      <Car/>
+      <Car/>
+      <Car/>
+      <Car/>
+      
+    </div>
+  )
+}
+
+
+
+
+
+
+
