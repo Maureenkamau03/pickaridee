5971e770282c21324b92431efeabac1e82169652
Ronny <ronnybet41@gmail.com>
2022-11-09 23:42:27 +0300

    add sidebar component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import "./header.css"
+
+export default function Header() {
+  return (
+    <div className="header ">
+    <div className="headertittle">
+       <span className="headtittlelg"> 
+       pickaride</span>
+    </div>
+    <img
+    className="headerimg"
+    src="car3.jpeg"
+    alt=""
+    ></img>
+</div>
+)
+}
+
