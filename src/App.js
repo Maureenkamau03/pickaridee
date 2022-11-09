3454df3bcdcf51af6c4fd803abb8d23e3bc718c6
Ronny <ronnybet41@gmail.com>
2022-11-09 23:43:08 +0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import "./App.css";
+
+import './App.css';
+import Home from './pages/home/Home';
+import TopBar from './components/topbar/Topbar';
+import Header from './components/header/Header';
 
 function App() {
-	return (
-		<div>
-			<h1>Hello world</h1>
-		</div>
-	);
+  return (
+    <div >
+      <TopBar/>
+      <Header/>
+      <Home/>
+    </div>
+  );
 }
 
 export default App;
