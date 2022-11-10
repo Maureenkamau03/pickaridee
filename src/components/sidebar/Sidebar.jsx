e3a3804128a44e05ea7734e42c25adba7fe2a4cd
Ronny <ronnybet41@gmail.com>
2022-11-10 16:04:37 +0300

    add changes

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,22 +1,26 @@
-import "./sidebar.css"
+import React from "react";
+import "./sidebar.css";
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
-    <div className="sidebaritem">
+      <div className="sidebaritem">
         <span className="sidebartittle">ABOUT PICKARIDE</span>
-        <img className="sidebarimg"
-            src="car5.jpeg" alt=""></img>
-        <p className="sidebarpara">Lorem ipsum dolor sit amet consectetur adipisicing elit. c
-            nulla iure! Laudantium vitae iure unde minus nihil dolore?</p>
-    </div>
-   
-    <div className="sidebaritem">
+        <img className="sidebarimg" src="car5.jpeg" alt=""></img>
+        <p className="sidebarpara">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. c nulla iure!
+          Laudantium vitae iure unde minus nihil dolore?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. c nulla iure!
+          Laudantium vitae iure unde minus nihil dolore?
+        </p>
+      </div>
+
+      <div className="sidebaritem">
         <span className="sidebartittle">FOLLOW US</span>
         <i className="sidebaricon fa-brands fa-facebook"></i>
         <i className="sidebaricon fa-brands fa-twitter"></i>
         <i className="sidebaricon fa-brands fa-instagram"></i>
+      </div>
     </div>
-</div>
-)
+  );
 }
